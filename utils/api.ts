d8995a28effe59ce042f117fed391baa90740ef3
Mike Melchior <mike.melchior@outlook.com>
2023-07-31 22:55:23 -0400

    create api call functions

---
+++ utils/api.ts
@@ -0,0 +1,13 @@
+export const getCategories = async () => {
+    let res = await fetch(`https://fakestoreapi.com/products/categories`)
+    if(!res.ok) throw new Error('Failed to fetch categories');
+    let data = await res.json();
+    return data
+}
+
+export const getProducts = async (category: string) => {
+    let res = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+    if (!res.ok) throw new Error('failed to fetch products')
+    let data = await res.json()
+    return data
+}
