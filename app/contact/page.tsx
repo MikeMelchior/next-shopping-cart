e888ab52cf72853d515ffbbfdcd382eee912b265
Mike Melchior <mike.melchior@outlook.com>
2023-08-02 13:20:21 -0400

    init contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <main> 
+        <div className='bg-clothes1 h-fill bg-cover'>
+            Contact Us
+        </div>
+    </main>
+  )
+}
+
+export default page
