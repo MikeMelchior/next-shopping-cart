034586fca3fcf79039b8517f68b5b3efa5a90074
Mike Melchior <mike.melchior@outlook.com>
2023-08-02 13:22:13 -0400

    add api call function for all products

--- utils/api.ts
+++ utils/api.ts
@@ -11,3 +11,10 @@ export const getProducts = async (category: string) => {
     let data = await res.json()
     return data
 }
+
+export const getAllProducts = async () => {
+    let res = await fetch(`https://fakestoreapi.com/products`);
+    if (!res.ok) throw new Error('Failed to fetch products');
+    let data = await res.json();
+    return data
+}
