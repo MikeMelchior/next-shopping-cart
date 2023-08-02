58fee361a0733293a00671b4a69810beca715987
Mike Melchior <mike.melchior@outlook.com>
2023-08-02 14:18:17 -0400

    disable decrement at 1

--- components/QuantityWidget.tsx
+++ components/QuantityWidget.tsx
@@ -4,24 +4,25 @@ import { Product } from '@/utils/types/Product'
 import React from 'react'
 
 
-const QuantityWidget = ( { product, count}: { product: Product, count: number}) => {
+const QuantityWidget = ( { product, count }: { product: Product, count: number}) => {
 
     const { removeItemFromCart, addItemToCart } = React.useContext(CartContext) as CartContextType
     return (
         <div className='flex rounded-xl text-lg'>
             <button 
-            className='border border-steel-blue px-4 text-2xl text-steel-blue hover:bg-blue-200 rounded-l-xl'
+                className='border border-r-0 border-steel-blue px-4 text-2xl text-steel-blue hover:bg-blue-200 rounded-l-xl disabled:opacity-30 disabled:hover:bg-transparent'
+                disabled={count === 1}
                 onClick={() => {
                     removeItemFromCart(product);
                 }}
             >
                 -
             </button>
-            <p className='py-1 px-4 border-t border-b border-steel-blue'>
+            <p className='py-1 px-4 border border-steel-blue'>
                 {count}
             </p>
             <button 
-                className='border border-steel-blue px-4 text-2xl text-steel-blue hover:bg-blue-200 rounded-r-xl'
+                className='border border-l-0 border-steel-blue px-4 text-2xl text-steel-blue hover:bg-blue-200 rounded-r-xl'
                 onClick={() => {
                     addItemToCart(product);
                 }}
