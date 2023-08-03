1a20b53b2aefe63c568b9f57e490cd353acbb6b7
Mike Melchior <mike.melchior@outlook.com>
2023-08-03 15:37:42 -0400

    adjust color

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,10 +5,10 @@ import { faTwitter, faFacebookF, faInstagram } from '@fortawesome/free-brands-sv
 const page = () => {
   return (
     <main className='bg-clothes1 h-fill bg-cover flex items-center justify-center'> 
-        <div className='flex flex-col p-6 gap-6 border border-black rounded-md bg-[rgba(231,231,231,0.5)]'>
-            <FontAwesomeIcon icon={faTwitter} className='h-24 m-auto' />
-            <FontAwesomeIcon icon={faFacebookF} className='h-24 m-auto' />
-            <FontAwesomeIcon icon={faInstagram} className='h-24 m-auto' />
+        <div className='flex flex-col p-6 gap-6 border border-black rounded-md bg-[rgba(231,231,231,0.3)]'>
+            <FontAwesomeIcon icon={faTwitter} className='h-24 m-auto cursor-pointer'/>
+            <FontAwesomeIcon icon={faFacebookF} className='h-24 m-auto cursor-pointer'/>
+            <FontAwesomeIcon icon={faInstagram} className='h-24 m-auto cursor-pointer'/>
         </div>
     </main>
   )
