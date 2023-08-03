eed1f5abb2fe608ce063ec071374e876bdf316b8
Mike Melchior <mike.melchior@outlook.com>
2023-08-03 13:01:08 -0400

    add properties to type

--- utils/types/CartContext.ts
+++ utils/types/CartContext.ts
@@ -3,6 +3,8 @@ import { Product } from "./Product"
 export type CartContextType = {
     cart: Product[]
     itemCount: number
+    itemInCart: (item: Product) => boolean
+    subtotal: number
     addItemToCart: (item: Product) => void
     removeItemFromCart: (item: Product) => void
     deleteProductFromCart: (item: Product) => void
