b2bbb7f5a4bf8c49e6d0b7d800278af47748737b
Mike Melchior <mike.melchior@outlook.com>
2023-08-03 12:58:06 -0400

    style component

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -5,24 +5,22 @@ import Image from 'next/image'
 import React from 'react'
 import QuantityWidget from './QuantityWidget'
 
-
 const CartItem = ( { item }: { item: Product }) => {
 
-    const { deleteProductFromCart, addItemToCart, removeItemFromCart } = React.useContext(CartContext) as CartContextType
+    const { deleteProductFromCart } = React.useContext(CartContext) as CartContextType
 
     return (
-        <div key={item.id} className='border-2 p-12 w-full flex gap-12 text-xl rounded-md'>
-            <Image src={item.image} alt='' width={200} height={200} className='w-[100px] h-[100px]'/>
-            
-            <div className='w-full flex flex-col'>
-                <div className='flex justify-between'>
+        <div key={item.id} className='bg-platinum border-2 p-12 w-full flex flex-wrap md:flex-nowrap gap-12 text-xl rounded-md'>
+            <Image src={item.image} alt='' width={200} height={200} className='min-w-[100px] min-h-[100px] w-[100px] h-[100px]'/>
+            <div className='w-full flex flex-col gap-6'>
+                <div className='flex flex-wrap md:flex-nowrap gap-6 justify-between'>
                     <p className='font-bold'>{item.title}</p>
                     <p>${(item.quantity * item.price).toFixed(2)}</p>
                 </div>
-                <div className='mt-auto flex justify-between'>
-                    <QuantityWidget product={item} count={item.quantity}/>
+                <div className='mt-auto flex flex-wrap gap-6 justify-between'>
+                    <QuantityWidget product={item}/>
                     <button 
-                        className='text-red-500 ml-auto mt-auto bg-pink-200 h-fit py-1 px-4 rounded-xl hover:animate-pulse'
+                        className='text-red-500 sm:ml-auto mt-auto bg-pink-200 h-fit py-1 px-4 rounded-xl hover:animate-pulse'
                         onClick={() => {
                             deleteProductFromCart(item)
                         }}
@@ -30,10 +28,7 @@ const CartItem = ( { item }: { item: Product }) => {
                         Remove
                     </button>
                 </div>
-                
             </div>
-            
-           
         </div>
     )
 }
