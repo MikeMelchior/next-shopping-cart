91bb5586ec3a2022f0bd35d1b6bb4a5a09f66ee3
Mike Melchior <mike.melchior@outlook.com>
2023-08-03 12:58:47 -0400

    style component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,18 +8,17 @@ type Props = {}
 const Navbar = (props: Props) => {
 
     const listStyles = 'hover:bg-platinum hover:text-rich-black h-full px-3 md:px-6 flex items-center transition-all duration-500'    
+
+
     return (
-        <nav className='bg-rich-black text-platinum text-xl md:text-2xl h-[80px] min-w-screen'>
-            <ul className='flex flex-wrap justify-between md:justify-normal h-full'>
-                <Link href={'/'} className='h-full'>
-                    <li className={`sm:hidden ${listStyles} mr-auto ${italiana.className} text-4xl`}>
+        <nav className='bg-rich-black text-platinum text-base sm:text-xl md:text-2xl h-[80px] min-w-screen'>
+            <ul className='flex justify-between md:justify-normal h-full'>
+                <Link href={'/'} className='h-full hidden sm:flex'>
+                    <li className={`${listStyles} mr-auto ${italiana.className} text-4xl`}>
                         FS
                     </li>
-                    <li className={`hidden sm:flex ${listStyles} mr-auto ${italiana.className} text-4xl`}>
-                        Fake Store
-                    </li>
                 </Link>
-                <Link href={'/'} className='ml-auto'>
+                <Link href={'/'} className='md:ml-auto'>
                     <li className={listStyles}>
                         Home
                     </li>
